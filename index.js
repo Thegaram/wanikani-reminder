5e16427f9860a99049f8b3603878a5f8cff5a1e1
Peter Garamvolgyi <peter.garamvolgyi@hotmail.com>
2020-10-03 15:53:36 +0200

    add messenger API webhook

--- index.js
+++ index.js
@@ -7,8 +7,55 @@ const app = express().use(bodyParser.json());
 const PORT = process.env.PORT || 1337;
 app.listen(PORT, () => console.log(`webhook is listening on port ${PORT}`));
 
+const VERIFY_TOKEN = process.env.MESSENGER_VERIFY_TOKEN;
+console.log(`VERIFY TOKEN = ${VERIFY_TOKEN}`);
+
+// healthcheck
 app.get('/ping', (req, res) => {
     res.status(200).send('pong');
+});
+
+// webhook verification
+app.get('/webhook', (req, res) => {
+    // parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // checks if a token and mode is in the query string of the request
+    if (mode && token) {
+        // checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            // responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            // responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
+});
+
+// webhook data
+app.post('/webhook', (req, res) => {
+    let body = req.body;
+
+    // checks this is an event from a page subscription
+    if (body.object === 'page') {
+        // iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
+            // gets the message. entry.messaging is an array, but
+            // will only ever contain one message, so we get index 0
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+        });
+
+        // returns a '200 OK' response to all requests
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        // Returns a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
 });
 
 function date_range_now() {
